d4ca7a0c0e16994ef66a47a2d1163e3c3163328d
Steven Kroman <steven.kroman@gmail.com>
2020-05-14 08:23:57 -0500

    v2 Debug 3

--- Shark_Data/app.py
+++ Shark_Data/app.py
@@ -17,7 +17,7 @@ import sys
 
 #insert at 1, 0 is the script path (or '' in REPL)
 sys.path.insert(1, os.path.join('static','py'))
-from .model import run_model
+from .model import model
 
 
 app = Flask(__name__)
